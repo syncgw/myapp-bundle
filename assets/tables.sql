c18e976885eeb7377f829752427d3e64fda63f26
syncgw <syncgw@github.com>
2024-04-15 13:17:46 +0200

    9.20.5

--- assets/tables.sql
+++ assets/tables.sql
@@ -3,7 +3,7 @@
 --
 --	@package	sync*gw
 --	@subpackage	myApp handler
---	@copyright	(c) 2008 - 2023 Florian Daeumling, Germany. All right reserved
+--	@copyright	(c) 2008 - 2024 Florian Daeumling, Germany. All right reserved
 -- 	@license 	LGPL-3.0-or-later
 --
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
